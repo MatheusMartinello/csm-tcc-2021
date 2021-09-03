c5239ee7ad46dd9bab279fc4f4c4b231147e38eb
Matheus Martinello <Matheus.martinello@grafftecnologia.com.br>
2021-09-03 18:59:15 -0300

    verificacao para bug2

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 //Routes
 app.use(express.json());
 app.use(routes);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server open in port 3000!");
