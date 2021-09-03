6d39fe5274c49241979bd0a0a50d96bff9908bfa
Matheus Martinello <Matheus.martinello@grafftecnologia.com.br>
2021-09-03 19:23:09 -0300

    NAO FAZ SENTIDO

--- src/app.js
+++ src/app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cors = require("cors");
-//Routes
-app.use(express.json());
-app.use(routes);
+//Routes`
 app.use(
   cors({
     origin: "*",
   })
 );
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server open in port 3000!");
